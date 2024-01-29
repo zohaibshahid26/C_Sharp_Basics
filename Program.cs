89cbb640ba2afdd26096b756e9be75215dee785c
Zohaib Shahid <zohaibshahid200@gmail.com>
2024-01-30 01:55:22 +0500

    Changes in files

--- Program.cs
+++ Program.cs
@@ -5,6 +5,22 @@ class Program
     {
         Console.WriteLine("Hello, World!");
         Console.WriteLine("This is my first C# program!");
+        Console.WriteLine("I am a student at The Tech Academy.");
+        Console.WriteLine("I am learning C#!");
+        Student student1 = new Student { Age = 12, Name = "Ali", Comment="he is a student", Grade=5};
+        Console.WriteLine(student1);
 
+        var x=add([1,2,4,5]);
+        Console.WriteLine(x);
+
+    }
+   static public int add(params int[] numbers)
+    {
+        int sum = 0;
+        foreach (int i in numbers)
+        {
+            sum += i;
+        }
+        return sum;
     }
 }
---
+++ Student.cs
@@ -0,0 +1,66 @@
+﻿
+class Student
+{
+    private string name;
+    private int age;
+    private int grade;
+    private string comment;
+    public Student()
+    {
+        name = "John Doe";
+        age = 0;
+        grade = 0;
+        comment = "No comment";
+    }
+ 
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+        set
+        {
+            name = Name;
+        }
+    }
+    public int Age
+    {
+        get
+        {
+            return age;
+        }
+        set
+        {
+            age = Age;
+        }
+    }
+    public int Grade
+    {
+        get
+        {
+            return grade;
+        }
+        set
+        {
+            grade = Grade;
+        }
+    }
+    public string Comment
+    {
+        get
+        {
+            return comment;
+        }
+        set
+        {
+            comment = Comment;
+        }
+    }
+  
+
+    public override string ToString()
+    {
+        return $"{Name} is {Age} years old. {Name} is in grade {Grade}. {Name}'s teacher says \"{Comment}\"";
+    }
+}
