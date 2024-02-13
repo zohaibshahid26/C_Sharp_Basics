b393bbd767c281af17ffd7b1875c243ed1472be9
Zohaib Shahid <zohaibshahid200@gmail.com>
2024-02-13 22:20:46 +0500

    Lecture 5 Contents

---
+++ Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Basics
+{
+    internal class Person
+    {
+
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int Age { get; set; }
+
+        // returning multiple values or tuple
+        public (string, string, int) GetPerson()
+        {
+            return (Name, Country, Age);
+        }
+    }
+}
